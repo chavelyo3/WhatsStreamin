f1e99b794b25c93e7075372307bb963562f3ea37
James Leitschuh <james.leitschuh02@gmail.com>
2021-01-16 13:25:14 -0600

    added watchlsit.js

--- models/watchlist.js
+++ models/watchlist.js
@@ -1,23 +1,27 @@
 module.exports = function(sequelize, DataTypes) {
-    var Post = sequelize.define("user", {
-      user: {
+    var Post = sequelize.define("watchlist", {
+      movie_title: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          len: [1,30]
+          len: [1,50]
         }
+      },
+      netflix: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false
       }
     });
   
     Post.associate = function(models) {
       // We're saying that a Post should belong to an Author
       // A Post can't be created without an Author due to the foreign key constraint
-      Post.belongsTo(models.watchlist, {
+      Post.belongsTo(models.user, {
         foreignKey: {
           allowNull: false
         }
       });
     };
   
-    return user;
+    return watchlist;
   };
