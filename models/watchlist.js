b94edb68d3ba0f3dbed00066753b1ac302280bc0
James Leitschuh <james.leitschuh02@gmail.com>
2021-01-19 18:49:02 -0600

    formatted user.js, watchlist.js, and server.js with eslint / prettier

--- models/watchlist.js
+++ models/watchlist.js
@@ -1,18 +1,19 @@
 module.exports = function(sequelize, DataTypes) {
-  let watchlist = sequelize.define(
+  const watchlist = sequelize.define(
     "watchlist",
     {
+      // eslint-disable-next-line camelcase
       movie_title: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          len: [1, 50],
-        },
+          len: [1, 50]
+        }
       },
       netflix: {
         type: DataTypes.BOOLEAN,
-        allowNull: false,
-      },
+        allowNull: false
+      }
     },
     { freezeTableName: true }
   );
@@ -22,8 +23,8 @@ module.exports = function(sequelize, DataTypes) {
     // A Post can't be created without an Author due to the foreign key constraint
     watchlist.belongsTo(models.user, {
       foreignKey: {
-        allowNull: true,
-      },
+        allowNull: true
+      }
     });
   };
 
