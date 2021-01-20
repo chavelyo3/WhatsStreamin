dccab56dd2cff0666433e44d32c7c820f8d29f87
James Leitschuh <james.leitschuh02@gmail.com>
2021-01-19 18:15:14 -0600

    Pulled from main, used ESLint and Prettier to format documents

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,33 +1,37 @@
-var db = require("../models");
+const db = require("../models");
 
 module.exports = function(app) {
-  app.get("/", function(req, res) {
+  app.get("/", (req, res) => {
     // 1. Add a join to include all of each Author's Posts
-    db.user.findAll({
-      // include: [db.watchlist]
-    }).then(function(data) {
-      const obj = {
-        title: "Home Page",
-        user: data
-    };
-      res.render("homepage",obj);
-    });
+    db.user
+      .findAll({
+        // include: [db.watchlist]
+      })
+      .then(data => {
+        const obj = {
+          title: "Home Page",
+          user: data
+        };
+        res.render("homepage", obj);
+      });
   });
 
-  app.get("/api/users/:id", function(req, res) {
+  app.get("/api/users/:id", (req, res) => {
     // 2; Add a join to include all of the Author's Posts here
-    db.user.findOne({
-      where: {
-        id: req.params.id
-      },
-     // include: [db.Post]
-    }).then(function(data) {
-      const obj = {
-        title: "Name",
-        user: data
-      };
-       res.render("user",obj);
-    });
+    db.user
+      .findOne({
+        where: {
+          id: req.params.id
+        }
+        // include: [db.Post]
+      })
+      .then(data => {
+        const obj = {
+          title: "Name",
+          user: data
+        };
+        res.render("user", obj);
+      });
   });
 
   // app.post("/api/authors", function(req, res) {
@@ -45,5 +49,4 @@ module.exports = function(app) {
   //     res.json(dbAuthor);
   //   });
   // });
-
 };
