6f56061a6363ec2c33f8054d6e5fef7803b5ee03
James Leitschuh <james.leitschuh02@gmail.com>
2021-01-20 11:19:25 -0600

    used eslint/prettier to clean up users.js

--- public/js/users.js
+++ public/js/users.js
@@ -1,19 +1,16 @@
-/* eslint-disable */
-
 $(document).ready(() => {
-    $(document).on("click","#submitBtn",addUser);
-    $(document).on("click",".delete-btn",deleteUser);
+  $(document).on("click", "#submitBtn", addUser);
+  $(document).on("click", ".delete-btn", deleteUser);
 
-    function addUser(event) {
-        event.preventDefault();
-        const name = $("#username").val();
-        const obj = { user : name};
+  function addUser(event) {
+    event.preventDefault();
+    const name = $("#username").val();
+    const obj = { user: name };
 
-        $.post("/api/users",obj)
-        .then(location.reload());
-    };
+    $.post("/api/users", obj).then(location.reload());
+  }
 
-    function deleteUser() {
-        alert("delete me");
-    };
+  function deleteUser() {
+    alert("delete me");
+  }
 });
