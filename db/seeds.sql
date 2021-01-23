5ca23899a316ff01a11c7ae0b148fdc977fade0a
zi wang <ziwang55@gmail.com>
2021-01-23 10:55:55 -0600

    adding delete button

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,14 @@
+USE streaming;
 INSERT INTO user (user) VALUES ('Molly');
+INSERT INTO user (user) VALUES ('James');
+INSERT INTO user (user) VALUES ('Beatrice');
+INSERT INTO user (user) VALUES ('testname');
+INSERT INTO user (user) VALUES ('sam');
+INSERT INTO user (user) VALUES ('zelda');
+
+USE streaming;
+INSERT INTO watchlist (movie_title,netflix,hulu,amazon,createdAt,UpdatedAt,userId) VALUES ("Test Movie",true,true,true,0,0,3);
+INSERT INTO watchlist (movie_title,netflix,hulu,amazon,createdAt,UpdatedAt,userId) VALUES ("best Movie",true,true,true,0,0,1);
+INSERT INTO watchlist (movie_title,netflix,hulu,amazon,createdAt,UpdatedAt,userId) VALUES ("yest Movie",true,true,true,0,0,1);
+INSERT INTO watchlist (movie_title,netflix,hulu,amazon,createdAt,UpdatedAt,userId) VALUES ("kest Movie",true,true,true,0,0,2);
+INSERT INTO watchlist (movie_title,netflix,hulu,amazon,createdAt,UpdatedAt,userId) VALUES ("pest Movie",true,true,true,0,0,3);
